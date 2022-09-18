4eb39c2b31b1b79d62045a70720042c916b80d2b
boraozenbirkan <b.ozenbirkan@gmail.com>
2022-09-18 21:21:36 +0300

    FDAO - Standards Changed
    I was re-implementing ERC20 standard but changed my approach to use normal standard and override the transfer functions to make it non-transferable. This way, we have token tracker as well.

--- FukcingDAO.sol
+++ FukcingDAO.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -63,253 +64,10 @@ import "@openzeppelin/contracts/utils/Counters.sol";
     We simply removed transfer, approve, allowance functions and events.
 */
 
-/**
- * @dev Interface of the Fukcing DAO.
- */
-interface IFDAO {
-    /*
-    function propose(string memory _decription) external;
-    
-    function vote(uint256 _proposalID, bool _isVotingFor) external;
-    
-    function lordVote(uint256 _proposalID, bool _isVotingFor) external;
-    
-    function newMint() external;
-
-    function claimToken(uint256 _mintProposalID) external;
-    
-    function proposalResult(uint256 _proposalID) external returns (bool);
-    */
-}
-
-/**
- * @dev Interface of the ERC20 standard as defined in the EIP. 
-    Removed transfer, approve, allowance functions and events.
- */
-interface IERC20 {
-
-    /**
-     * @dev Removed transfer, approve, allowance functions and events.
-     */
-
-    /**
-     * @dev Returns the amount of tokens in existence.
-     */
-    function totalSupply() external view returns (uint256);
-
-    /**
-     * @dev Returns the amount of tokens owned by `account`.
-     */
-    function balanceOf(address account) external view returns (uint256);
-}
-
-/**
- * @dev Interface for the optional metadata functions from the ERC20 standard.
- *
- * _Available since v4.1._
- */
-interface IERC20Metadata is IERC20 {
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() external view returns (string memory);
-
-    /**
-     * @dev Returns the symbol of the token.
-     */
-    function symbol() external view returns (string memory);
-
-    /**
-     * @dev Returns the decimals places of the token.
-     */
-    function decimals() external view returns (uint8);
-}
-
-/**
- * @dev Implementation of the {IERC20} interface.
- *
- * This implementation is agnostic to the way tokens are created. This means
- * that a supply mechanism has to be added in a derived contract using {_mint}.
- * For a generic mechanism see {ERC20PresetMinterPauser}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
- *
- * We have followed general OpenZeppelin Contracts guidelines: functions revert
- * instead returning `false` on failure. This behavior is nonetheless
- * conventional and does not conflict with the expectations of ERC20
- * applications.
- */
-contract ERC20 is Context, IERC20, IERC20Metadata {
-
-    /**
-     * @dev Removed transfer, approve, allowance functions and events.        
-    */
-
-
-    mapping(address => uint256) private _balances;
-
-    mapping(address => mapping(address => uint256)) private _allowances;
-
-    uint256 private _totalSupply;
-
-    string private _name;
-    string private _symbol;
-
-    /**
-     * @dev Sets the values for {name} and {symbol}.
-     *
-     * The default value of {decimals} is 18. To select a different value for
-     * {decimals} you should overload it.
-     *
-     * All two of these values are immutable: they can only be set once during
-     * construction.
-     */
-    constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-
-    /**
-     * @dev Returns the name of the token.
-     */
-    function name() public view virtual override returns (string memory) {
-        return _name;
-    }
-
-    /**
-     * @dev Returns the symbol of the token, usually a shorter version of the
-     * name.
-     */
-    function symbol() public view virtual override returns (string memory) {
-        return _symbol;
-    }
-
-    /**
-     * @dev Returns the number of decimals used to get its user representation.
-     * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
-     *
-     * Tokens usually opt for a value of 18, imitating the relationship between
-     * Ether and Wei. This is the value {ERC20} uses, unless this function is
-     * overridden;
-     *
-     * NOTE: This information is only used for _display_ purposes: it in
-     * no way affects any of the arithmetic of the contract, including
-     * {IERC20-balanceOf} and {IERC20-transfer}.
-     */
-    function decimals() public view virtual override returns (uint8) {
-        return 18;
-    }
-
-    /**
-     * @dev See {IERC20-totalSupply}.
-     */
-    function totalSupply() public view virtual override returns (uint256) {
-        return _totalSupply;
-    }
-
-    /**
-     * @dev See {IERC20-balanceOf}.
-     */
-    function balanceOf(address account) public view virtual override returns (uint256) {
-        return _balances[account];
-    }
-    
-
-    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
-     * the total supply.
-     *
-     * Emits a {Transfer} event with `from` set to the zero address.
-     *
-     * Requirements:
-     *
-     * - `account` cannot be the zero address.
-     */
-    function _mint(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: mint to the zero address");
-
-        _beforeTokenTransfer(address(0), account, amount);
-
-        _totalSupply += amount;
-        _balances[account] += amount;
-
-        _afterTokenTransfer(address(0), account, amount);
-    }
-
-    /**
-     * @dev Destroys `amount` tokens from `account`, reducing the
-     * total supply.
-     *
-     * Emits a {Transfer} event with `to` set to the zero address.
-     *
-     * Requirements:
-     *
-     * - `account` cannot be the zero address.
-     * - `account` must have at least `amount` tokens.
-     */
-    function _burn(address account, uint256 amount) internal virtual {
-        require(account != address(0), "ERC20: burn from the zero address");
-
-        _beforeTokenTransfer(account, address(0), amount);
-
-        uint256 accountBalance = _balances[account];
-        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
-        unchecked {
-            _balances[account] = accountBalance - amount;
-        }
-        _totalSupply -= amount;
-
-        _afterTokenTransfer(account, address(0), amount);
-    }
-
-
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {}
-
-    /**
-     * @dev Hook that is called after any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * has been transferred to `to`.
-     * - when `from` is zero, `amount` tokens have been minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens have been burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    function _afterTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual {}
-}
-
 /*
  * @author Bora Ozenbirkan
  */
-contract FukcingDAO is ERC20, AccessControl, IFDAO {
+contract FukcingDAO is ERC20, AccessControl {
     using Counters for Counters.Counter;   
 
     enum ProposalStatus{
@@ -361,6 +119,29 @@ contract FukcingDAO is ERC20, AccessControl, IFDAO {
         _grantRole(EXECUTER_ROLE, msg.sender);
 
         initializeProposelTypes();
+    }
+
+    /*
+     *  @dev Making token non-transferable by overriding all the transfer functions
+     *
+    */
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        revert("This is a non-transferable token!");
+    }
+    function allowance(address owner, address spender) public virtual override view returns (uint256) {
+        revert("This is a non-transferable token!");
+    }
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        revert("This is a non-transferable token!");
+    }
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        revert("This is a non-transferable token!");
+    }
+    function increaseAllowance(address spender, uint256 addedValue) public virtual override returns (bool) {
+        revert("This is a non-transferable token!");
+    }
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override returns (bool) {
+        revert("This is a non-transferable token!");
     }
     
     /*
@@ -505,6 +286,8 @@ contract FukcingDAO is ERC20, AccessControl, IFDAO {
         proposal.participants++;
         proposal.totalVotes += votingPower;        
     }
+    
+    // Add spending functions
 
     function proposalResult (uint256 _proposalID) public returns(uint256){
         Proposal storage proposal = proposals[_proposalID];
