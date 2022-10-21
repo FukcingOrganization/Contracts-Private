88766d35c052b72fb1b78caee108a7df6e430146
boraozenbirkan <b.ozenbirkan@gmail.com>
2022-10-21 12:43:24 +0300

    FEXE - Assign/Resign Executers
    - Proposal and execution function is done.
    - Update function standards have simplified and get better format to use.

--- FukcingExecuters.sol
+++ FukcingExecuters.sol
@@ -41,8 +41,8 @@ contract FukcingExecuters is Context, AccessControl {
     bool newBool;
   }
 
-  mapping(uint256 => Proposal) public proposals;          // proposalID => Proposal
-  mapping(address => bool) public isExecutor;  // The executors
+  mapping(uint256 => Proposal) public proposals;  // proposalID => Proposal
+  mapping(address => bool) public isExecutor;     // The executors
 
   bytes32 public constant EXECUTER_ROLE = keccak256("EXECUTER_ROLE");
 
@@ -73,29 +73,40 @@ contract FukcingExecuters is Context, AccessControl {
   }
 
 
-  function updateContractAddress(
+  function createContractAddressUpdateProposal(
     uint256 contractIndex,  // Destination Contract address
     uint256 subjectIndex,   // The address that we want to update in the destination contract. Same index as contracts
-    address newAddress,     // New address
-    bool isNewProposal      
+    address newAddress     // New address
   ) public onlyRole(EXECUTER_ROLE) {
     (bool txSuccess, ) = contracts[contractIndex].call(abi.encodeWithSignature(
-      "updateContractAddress(uint256,address,bool)", subjectIndex, newAddress, isNewProposal
+      "proposeContractAddressUpdate(uint256,address,bool)", subjectIndex, newAddress
     ));
     require(txSuccess, "Transaction failed to execute update function!");   
   }
 
-  // 
+  
+  /**
+   * Updates by DAO - Update Codes
+   * Executer Propsosal -> Code: 1
+   * xxxxx -> Code: 2
+   * yyyyy -> Code: 3
+   * zzzzz -> Code: 4
+   * 
+  **/
 
   // Proposals to update the state of the executor contract
 
-  function setExecuter(address _address, bool _setAsExecuter, bool isNewProposal) public {
+  function proposeExecuterRole(address _address, bool _setAsExecuter) public {
+    require(_address != address(0), "The executor address can not be the null address!");
+
     // require executor status is not already set
     if (_setAsExecuter)
       require(hasRole(EXECUTER_ROLE, _address) == false, "The address is already an executor!");
     else
       require(hasRole(EXECUTER_ROLE, _address) == true, "The address is already NOT an executor!");
 
+
+    /// *************** Check Caller's Eligibility to Propose *************** ///
 
     // Only addresses who hold required amount of governance token (FDAO) can call this function
     // Get required balance to propose
@@ -113,58 +124,57 @@ contract FukcingExecuters is Context, AccessControl {
     (uint256 callerBalance) = abi.decode(returnData1, (uint256));
 
     require(callerBalance >= requiredBalance, "You don't have enough FDAO tokens to call this function!");
+
         
+    /// ************************** Create Proposal ************************** ///
 
-    // if PID[4] is 0, make a new proposal
-    if (proposalIdTracker[4] == 0 && isNewProposal) { // Which is default
-      require(_address != address(0), "The executor address can not be the null address!");
             
-      string memory proposalDescription;
-      if (_setAsExecuter){
-        proposalDescription = string(abi.encodePacked(
-          "Assigning executor status of ", Strings.toHexString(_address), " address as TRUE"
-        ));
-      }
-      else {
-        proposalDescription = string(abi.encodePacked(
-          "Assigning executor status of ", Strings.toHexString(_address), " address as FALSE"
-        ));
-      } 
-
-      // Create a new proposal
-      (bool txSuccess2, bytes memory returnData2) = contracts[4].call(
-        abi.encodeWithSignature("newProposal(string,uint256)", proposalDescription, proposalType)
-      );
-      require(txSuccess2, "Transaction failed to make new proposal!");
-
-      // Save the ID to proposalIdTracker[4]
-      (proposalIdTracker[4]) = abi.decode(returnData2, (uint256));
-
-      // Get new state update by proposal ID we get from newProposal
-      proposals[proposalIdTracker[4]].newAddress = _address;
-      proposals[proposalIdTracker[4]].newBool = _setAsExecuter;
-
-      return; // Finish the function
+    string memory proposalDescription;
+    if (_setAsExecuter){
+      proposalDescription = string(abi.encodePacked(
+        "Assigning ", Strings.toHexString(_address), " address as a new executer!"
+      ));
     }
+    else {
+      proposalDescription = string(abi.encodePacked(
+        "Resigning ", Strings.toHexString(_address), " address from its executer role!"
+      ));
+    } 
 
-    // If there is already a proposal, Get its result from DAO
-    (bool txSuccess3, bytes memory returnData3) = contracts[4].call(
-      abi.encodeWithSignature("proposalResult(uint256)", proposalIdTracker[4])
+    // Create a new proposal
+    (bool txSuccess2, bytes memory returnData2) = contracts[4].call(
+      abi.encodeWithSignature("newProposal(string,uint256)", proposalDescription, proposalType)
     );
-    require(txSuccess3, "Transaction failed to make new proposal!");
+    require(txSuccess2, "Transaction failed to make new proposal!");
 
-    // Save it here
-    Proposal storage proposal = proposals[proposalIdTracker[4]];
-    (uint256 statusNum) = abi.decode(returnData3, (uint256));
+    // Get the ID
+    (uint256 propID) = abi.decode(returnData2, (uint256));
+
+    // Save data to the proposal
+    proposals[propID].updateCode = 1;
+    proposals[propID].newAddress = _address;
+    proposals[propID].newBool = _setAsExecuter;
+  }
+
+  function executeRoleProposal(uint256 _proposalID) public {
+    Proposal storage proposal = proposals[_proposalID];
+
+    require(proposal.updateCode == 1 || proposal.isExecuted == false, "Wrong proposal ID");
+
+    // Get the proposal result from DAO
+    (bool txSuccess, bytes memory returnData) = contracts[4].call(
+      abi.encodeWithSignature("proposalResult(uint256)", _proposalID)
+    );
+    require(txSuccess, "Transaction failed to make new proposal!");
+
+    // Save the result here
+    (uint256 statusNum) = abi.decode(returnData, (uint256));
     proposal.status = Status(statusNum);
 
-    // Wait for the current one to finalize
-    string memory errorText = string(abi.encodePacked("The previous proposal is still going on bro.", 
-      " Wait for the DAO decision on the proposal! The proposal ID = ", Strings.toString(proposalIdTracker[4]), "."
-    )); 
-    require(uint256(proposal.status) > 1, errorText);
+    // Check if it is finalized or not
+    require(uint256(proposal.status) > 1, "The proposal still going on or not even started!");
 
-    // if the current one is approved, take action according to newBool which is _setAsExecuter
+    // if the proposal is approved, apply the update the state
     if (proposal.status == Status.Approved){
       if (proposal.newBool == true)
         _grantRole(EXECUTER_ROLE, proposal.newAddress);
@@ -172,7 +182,7 @@ contract FukcingExecuters is Context, AccessControl {
         _revokeRole(EXECUTER_ROLE, proposal.newAddress);
     }
 
-    proposalIdTracker[4] = 0;   // reset proposal tracker
+    proposal.isExecuted = true;
   }
 
 }
